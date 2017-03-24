23009066c389649c1668160f53d5a82bf75ce196
antyang <mrant0ny@gmail.com>
2017-03-23 20:34:26 -0700

    Add Cell tests
    * has 8 neighbors
    * is dead by default
    * should die if it has fewer than 2 live neighbors
    * should live on if it has 2 or 3 live neighbors
    * should die if it has more than 3 neighbors
    * should be born if it has exactly 3 neighbors

--- cell.js
+++ cell.js
@@ -1,12 +1,39 @@
 var Cell = function() {
   var me = this;
 
-  me.neighbors = [];
+  me.neighbors = Array.apply(null, Array(8));
+
+  me.isAlive = false;
 
   me.getNeighbors = function() {
     return me.neighbors;
   }
 
+  me.shouldDie = function() {
+    var livingNeighbors = me.neighbors.filter(function(c) {
+      return c.isAlive;
+    });
+
+    if(livingNeighbors.length < 2) {
+      return true;
+    }
+    if(livingNeighbors.length > 3) {
+      return true;
+    }
+
+    return false;
+  }
+
+  me.shouldBeBorn = function() {
+    var livingNeighbors = me.neighbors.filter(function(c) {
+      return c.isAlive;
+    });
+
+    if(livingNeighbors.length === 3) {
+      return true;
+    }
+    return false;
+  }
 
   return me;
 }
--- tests/cellspec.js
+++ tests/cellspec.js
@@ -6,14 +6,53 @@ describe("A cell", function() {
   // before each test
   beforeEach(function() {
     cell = new Cell();
+    cell.neighbors = cell.neighbors.map(function() {
+      return new Cell();
+    })
   });
 
   it('is defined', function() {
     expect(Cell).toBeDefined();
   });
 
-  it('has neighbors', function() {
+  it('has 8 neighbors', function() {
     var neighbors = cell.getNeighbors();
     expect(neighbors).toBeTruthy();
+
+    expect(neighbors.length).toBe(8);
+  });
+
+  it('is dead by default', function() {
+    expect(cell.isAlive).toBe(false);
+  });
+
+  it('should die if it has fewer than 2 live neighbors', function() {
+    cell.neighbors[0].isAlive = true;
+
+    expect(cell.shouldDie()).toBe(true);
+  });
+
+  it('should live on if it has 2 or 3 live neighbors', function() {
+    cell.neighbors[0].isAlive = true;
+    cell.neighbors[1].isAlive = true;
+    expect(cell.shouldDie()).toBe(false);
+
+    cell.neighbors[2].isAlive = true;
+    expect(cell.shouldDie()).toBe(false);
+  });
+
+  it('should die if it has more than 3 neighbors', function() {
+    cell.neighbors[0].isAlive = true;
+    cell.neighbors[1].isAlive = true;
+    cell.neighbors[2].isAlive = true;
+    cell.neighbors[3].isAlive = true;
+    expect(cell.shouldDie()).toBe(true);
+  });
+
+  it('should be born if it has exactly 3 neighbors', function() {
+    cell.neighbors[0].isAlive = true;
+    cell.neighbors[1].isAlive = true;
+    cell.neighbors[2].isAlive = true;
+    expect(cell.shouldBeBorn()).toBe(true);
   });
 })
