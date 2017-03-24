d01ca55c985154e913c9abf1cb486a2cf8078662
antyang <mrant0ny@gmail.com>
2017-03-23 18:24:06 -0700

    Add test
    * Pass test - 'A cell has neighbors'

--- cell.js
+++ cell.js
@@ -1,2 +1,12 @@
 var Cell = function() {
+  var me = this;
+
+  me.neighbors = [];
+
+  me.getNeighbors = function() {
+    return me.neighbors;
+  }
+
+
+  return me;
 }
--- tests/cellspec.js
+++ tests/cellspec.js
@@ -1,7 +1,19 @@
 /// jasmine test runner
 
 describe("A cell", function() {
+  var cell;
+
+  // before each test
+  beforeEach(function() {
+    cell = new Cell();
+  });
+
   it('is defined', function() {
     expect(Cell).toBeDefined();
-  })
+  });
+
+  it('has neighbors', function() {
+    var neighbors = cell.getNeighbors();
+    expect(neighbors).toBeTruthy();
+  });
 })
